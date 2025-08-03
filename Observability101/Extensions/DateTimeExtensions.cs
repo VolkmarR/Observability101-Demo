4ede1592717a5282c71c7227f0fa0d14b5223137
Volkmar Rigo <Volkmar.rigo@Infominds.eu>
2025-08-03 20:54:57 +0200

    Added Extension for DateTime

---
+++ Observability101/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace Observability101.Extensions;
+
+public static class DateTimeExtensions
+{
+    public static DateTime TruncateTimeToMinute(this DateTime timeStamp) => new(timeStamp.Year, timeStamp.Month,
+        timeStamp.Day, timeStamp.Hour, timeStamp.Minute, 0, timeStamp.Kind);
+}
