913219273d833feda08e34117f2bffe2dbe28ebe
Volkmar Rigo <VolkmarR@users.noreply.github.com>
2025-08-10 20:28:14 +0200

    Setup (#1)
    * Initial setup of fast-endpoints
    * Added OTEL Config
    * Fixed issue with http calls
    * Added Extension for DateTime
    * Added Database
    * Refactorings and Activitiy examples

---
+++ Observability101/Infrastructure/Database/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Observability101.Infrastructure.Database;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Weather> Weather { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new WeatherConfiguration());
+    }
+}
---
+++ Observability101/OpenTelemetry/CustomActivitiySource.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using OpenTelemetry.Trace;
+
+namespace Observability101.OpenTelemetry;
+
+public static class CustomSourceForTracing
+{
+    private const string CustomTraceKey = "CustomActivity";
+
+    /// <summary>
+    /// Static ActivitySource instance used to add custom spans
+    /// </summary>
+    public static ActivitySource Source { get; } = new ActivitySource(CustomTraceKey, "1.0.0");
+
+    /// <summary>
+    /// Adds custom tracing support to the TracerProviderBuilder.
+    /// </summary>
+    /// <param name="builder">The TracerProviderBuilder instance to which the custom tracing source will be added.</param>
+    /// <returns>The updated TracerProviderBuilder instance with the custom tracing source configured.</returns>
+    public static TracerProviderBuilder AddCustomTracing(this TracerProviderBuilder builder)
+        => builder.AddSource(CustomTraceKey);
+}
